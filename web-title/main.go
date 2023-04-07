278c75695d7897c7180feb9bb9b046751665b409
riversteve <56084920+riversteve@users.noreply.github.com>
2023-04-07 01:52:07 -0400

    Add handleError function

--- web-title/main.go
+++ web-title/main.go
@@ -12,53 +12,49 @@ import (
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Println("Usage: web-title [url]")
-        os.Exit(1)
-    }
-	
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], " [url]")
+		os.Exit(1)
+	}
+
 	url := os.Args[1]
 	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
 		url = "https://" + url
 	}
-    resp, err := http.Get(url)
 
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	resp, err := http.Get(url)
+	handleError(err)
 
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	handleError(err)
 
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
+	doc, err := html.Parse(bytes.NewReader(body))
+	handleError(err)
 
-    doc, err := html.Parse(bytes.NewReader(body))
+	title := getTitle(doc)
+	fmt.Println(title)
+}
 
-    if err != nil {
-        fmt.Println("Error:", err)
-        os.Exit(1)
-    }
-
-    title := getTitle(doc)
-    fmt.Println(title)
+func handleError(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
 
 func getTitle(doc *html.Node) string {
-    if doc.Type == html.ElementNode && doc.Data == "title" {
-        return doc.FirstChild.Data
-    }
+	if doc.Type == html.ElementNode && doc.Data == "title" {
+		return doc.FirstChild.Data
+	}
 
-    for c := doc.FirstChild; c != nil; c = c.NextSibling {
-        title := getTitle(c)
+	for c := doc.FirstChild; c != nil; c = c.NextSibling {
+		title := getTitle(c)
 
-        if title != "" {
-            return title
-        }
-    }
+		if title != "" {
+			return title
+		}
+	}
 
-    return ""
+	return ""
 }
