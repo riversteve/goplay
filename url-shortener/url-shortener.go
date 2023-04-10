37676c14cf5ec61bf8dd50df66229d435369570a
riversteve <56084920+riversteve@users.noreply.github.com>
2023-04-10 00:24:19 -0400

    Welcoem GTP4

--- url-shortener/url-shortener.go
+++ url-shortener/url-shortener.go
@@ -5,11 +5,14 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/boltdb/bolt"
 )
 
 var (
-	urls    map[string]string = make(map[string]string)
-	letters                   = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	db      *bolt.DB
 )
 
 func shortenUrl(w http.ResponseWriter, r *http.Request) {
@@ -23,15 +26,19 @@ func shortenUrl(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	shortUrl := generateShortUrl()
-	urls[shortUrl] = longUrl
+	err := saveUrl(shortUrl, longUrl)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusCreated)
 	fmt.Fprintf(w, "Shortened URL: http://%s/%s\n", r.Host, shortUrl)
 }
 
 func redirectToLongUrl(w http.ResponseWriter, r *http.Request) {
 	shortUrl := r.URL.Path[1:]
-	longUrl, ok := urls[shortUrl]
-	if !ok {
+	longUrl, err := loadUrl(shortUrl)
+	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
@@ -46,14 +53,53 @@ func generateShortUrl() string {
 			b[i] = letters[rand.Intn(len(letters))]
 		}
 		shortUrl = string(b)
-		if _, ok := urls[shortUrl]; !ok {
+		_, err := loadUrl(shortUrl)
+		if err != nil {
 			break
 		}
 	}
 	return shortUrl
 }
 
+func saveUrl(shortUrl, longUrl string) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("urls"))
+		return b.Put([]byte(shortUrl), []byte(longUrl))
+	})
+}
+
+func loadUrl(shortUrl string) (string, error) {
+	var longUrl string
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("urls"))
+		v := b.Get([]byte(shortUrl))
+		if v == nil {
+			return fmt.Errorf("URL not found")
+		}
+		longUrl = string(v)
+		return nil
+	})
+	return longUrl, err
+}
+
 func main() {
+	var err error
+	db, err = bolt.Open("urls.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("urls"))
+		return err
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
 	http.HandleFunc("/shorten", shortenUrl)
 	http.HandleFunc("/", redirectToLongUrl)
 	log.Fatal(http.ListenAndServe(":8080", nil))
