7f89ed58f9cc21b7d06fe0b6b622d38904c7df67
riversteve <56084920+riversteve@users.noreply.github.com>
2023-04-10 23:56:47 -0400

    Better tests

--- url-shortener/url-shortener_test.go
+++ url-shortener/url-shortener_test.go
@@ -1,11 +1,46 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/boltdb/bolt"
 )
+
+func TestMain(m *testing.M) {
+	// Set up the database for testing
+	testDB, err := bolt.Open("test_urls.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove("test_urls.db")
+	defer testDB.Close()
+
+	err = testDB.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("urls"))
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// Temporarily set the global 'db' variable to the test database
+	origDB := db
+	db = testDB
+
+	// Run tests
+	exitCode := m.Run()
+
+	// Restore the original 'db' variable
+	db = origDB
+
+	// Exit with the test result exit code
+	os.Exit(exitCode)
+}
 
 func TestGenerateShortUrl(t *testing.T) {
 	shortUrl := generateShortUrl()
@@ -45,5 +80,63 @@ func TestSaveAndLoadUrl(t *testing.T) {
 	})
 	if err != nil {
 		t.Errorf("Failed to clean up test entry: %v", err)
+	}
+}
+
+func TestSaveLoadAndDeleteUrl(t *testing.T) {
+	testShortUrl := "test123"
+	testLongUrl := "https://example.com"
+
+	err := saveUrl(testShortUrl, testLongUrl)
+	if err != nil {
+		t.Fatalf("Failed to save URL: %v", err)
+	}
+
+	longUrl, err := loadUrl(testShortUrl)
+	if err != nil {
+		t.Fatalf("Failed to load URL: %v", err)
+	}
+
+	if longUrl != testLongUrl {
+		t.Errorf("Expected long URL %q but got %q", testLongUrl, longUrl)
+	}
+
+	err = removeUrl(testShortUrl)
+	if err != nil {
+		t.Fatalf("Failed to delete URL: %v", err)
+	}
+
+	_, err = loadUrl(testShortUrl)
+	if err == nil {
+		t.Fatalf("URL should not be found after deletion")
+	}
+}
+
+func TestDeleteUrlHandler(t *testing.T) {
+	testShortUrl := "test456"
+	testLongUrl := "https://example.org"
+
+	err := saveUrl(testShortUrl, testLongUrl)
+	if err != nil {
+		t.Fatalf("Failed to save URL: %v", err)
+	}
+
+	req, err := http.NewRequest("DELETE", "/delete", strings.NewReader("shortUrl="+testShortUrl))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(deleteUrl)
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+
+	_, err = loadUrl(testShortUrl)
+	if err == nil {
+		t.Fatalf("URL should not be found after deletion")
 	}
 }
